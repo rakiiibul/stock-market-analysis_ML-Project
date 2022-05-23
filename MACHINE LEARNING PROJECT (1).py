6ebff102e1148818bc770ccd33eee3d0ea2f4198
Rukhsarahmed1 <92302055+Rukhsarahmed1@users.noreply.github.com>
2022-05-23 12:42:14 +0200

    Ml

---
+++ MACHINE LEARNING PROJECT (1).py
@@ -0,0 +1,412 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# first time you need to install the following packages 
+# pip install DataReader for getting the data from yahoo finance
+# pip install yfinance for access the data from yahoo stock market
+# pip install keras
+# pip install tensorflow
+
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+# Data Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+# For reading stock data from yahoo
+from __future__ import division
+from pandas_datareader import DataReader 
+import yfinance as yf
+
+
+# For time stamps
+from datetime import datetime
+
+
+# In[3]:
+
+
+tech_list = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+# Set up End and Start times for data grab
+tech_list = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+end = datetime.now()
+start = datetime(end.year - 1, end.month, end.day)
+
+for stock in tech_list:
+    globals()[stock] = yf.download(stock, start, end)
+
+
+# In[4]:
+
+
+company_list = [AAPL, GOOG, MSFT, AMZN]
+company_name = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+
+for company, com_name in zip(company_list, company_name):
+    company["company_name"] = com_name
+    
+df = pd.concat(company_list, axis=0)
+df.tail(10)
+
+
+# In[5]:
+
+
+# Summary Stats
+GOOG.describe()
+
+
+# In[6]:
+
+
+# General info
+GOOG.info()
+
+
+# In[7]:
+
+
+# Let's see a historical view of the closing price
+
+
+plt.figure(figsize=(15, 6))
+plt.subplots_adjust(top=1.25, bottom=1.2)
+
+for i, company in enumerate(company_list, 1):
+    plt.subplot(2, 2, i)
+    company['Adj Close'].plot()
+    plt.ylabel('Adj Close')
+    plt.xlabel(None)
+    plt.title(f"Closing Price of {tech_list[i - 1]}")
+    
+plt.tight_layout()
+
+
+# In[8]:
+
+
+ma_day = [10, 20, 50]
+
+for ma in ma_day:
+    for company in company_list:
+        column_name = f"MA for {ma} days"
+        company[column_name] = company['Adj Close'].rolling(ma).mean()
+
+
+# In[9]:
+
+
+fig, axes = plt.subplots(nrows=2, ncols=2)
+fig.set_figheight(8)
+fig.set_figwidth(15)
+
+AAPL[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[0,0])
+axes[0,0].set_title('APPLE')
+
+GOOG[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[0,1])
+axes[0,1].set_title('GOOGLE')
+
+MSFT[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[1,0])
+axes[1,0].set_title('MICROSOFT')
+
+AMZN[['Adj Close', 'MA for 10 days', 'MA for 20 days', 'MA for 50 days']].plot(ax=axes[1,1])
+axes[1,1].set_title('AMAZON')
+
+fig.tight_layout()
+
+
+# In[10]:
+
+
+# We'll use pct_change to find the percent change for each day
+for company in company_list:
+    company['Daily Return'] = company['Adj Close'].pct_change()
+
+# Then we'll plot the daily return percentage
+fig, axes = plt.subplots(nrows=2, ncols=2)
+fig.set_figheight(8)
+fig.set_figwidth(15)
+
+AAPL['Daily Return'].plot(ax=axes[0,0], legend=True, linestyle='--', marker='o')
+axes[0,0].set_title('APPLE')
+
+GOOG['Daily Return'].plot(ax=axes[0,1], legend=True, linestyle='--', marker='o')
+axes[0,1].set_title('GOOGLE')
+
+MSFT['Daily Return'].plot(ax=axes[1,0], legend=True, linestyle='--', marker='o')
+axes[1,0].set_title('MICROSOFT')
+
+AMZN['Daily Return'].plot(ax=axes[1,1], legend=True, linestyle='--', marker='o')
+axes[1,1].set_title('AMAZON')
+
+fig.tight_layout()
+
+
+# # Now let's get an overall look at the average daily return using a histogram. We'll use seaborn to create both a histogram and kde plot on the same figure.
+
+# In[11]:
+
+
+plt.figure(figsize=(12, 7))
+
+for i, company in enumerate(company_list, 1):
+    plt.subplot(2, 2, i)
+    company['Daily Return'].hist(bins=50)
+    plt.ylabel('Daily Return')
+    plt.title(f'{company_name[i - 1]}')
+    
+plt.tight_layout()
+
+
+# # 4. What was the correlation between different stocks closing prices?
+
+# In[12]:
+
+
+# Grab all the closing prices for the tech stock list into one DataFrame
+closing_df = DataReader(tech_list, 'yahoo', start, end)['Adj Close']
+
+# Let's take a quick look
+closing_df.head() 
+
+
+# Now that we have all the closing prices, let's go ahead and get the daily return for all the stocks, like we did for the Apple stock.
+
+# In[13]:
+
+
+# Make a new tech returns DataFrame
+tech_rets = closing_df.pct_change()
+tech_rets.head()
+
+
+# Now we can compare the daily percentage return of two stocks to check how correlated. First let's see a sotck compared to itself.
+
+# In[14]:
+
+
+# Comparing Google to itself should show a perfectly linear relationship
+sns.jointplot(x='GOOG', y='GOOG', data=tech_rets, kind='scatter', color='seagreen')
+
+
+# In[15]:
+
+
+# We'll use joinplot to compare the daily returns of Google and Microsoft
+sns.jointplot(x='GOOG', y='MSFT', data=tech_rets, kind='scatter')
+
+
+# So now we can see that if two stocks are perfectly (and positivley) correlated with each other a linear relationship bewteen its daily return values should occur.
+# 
+# Seaborn and pandas make it very easy to repeat this comparison analysis for every possible combination of stocks in our technology stock ticker list. We can use sns.pairplot() to automatically create this plot
+
+# In[16]:
+
+
+# We can simply call pairplot on our DataFrame for an automatic visual analysis 
+# of all the comparisons
+
+sns.pairplot(tech_rets, kind='reg')
+
+
+# In[17]:
+
+
+# Let's go ahead and use sebron for a quick correlation plot for the daily returns
+sns.heatmap(tech_rets.corr(), annot=True, cmap='summer')
+
+
+# In[18]:
+
+
+sns.heatmap(closing_df.corr(), annot=True, cmap='summer')
+
+
+# # How much value do we put at risk by investing in a particular stock
+
+# In[19]:
+
+
+# Let's start by defining a new DataFrame as a clenaed version of the oriignal tech_rets DataFrame
+rets = tech_rets.dropna()
+
+area = np.pi * 20
+
+plt.figure(figsize=(10, 7))
+plt.scatter(rets.mean(), rets.std(), s=area)
+plt.xlabel('Expected return')
+plt.ylabel('Risk')
+
+for label, x, y in zip(rets.columns, rets.mean(), rets.std()):
+    plt.annotate(label, xy=(x, y), xytext=(50, 50), textcoords='offset points', ha='right', va='bottom', 
+                 arrowprops=dict(arrowstyle='-', color='blue', connectionstyle='arc3,rad=-0.3'))
+
+
+# # Predicting the closing price stock price of Google
+
+# In[20]:
+
+
+# Get the stock quote
+df = DataReader('GOOG', data_source='yahoo', start='2012-01-01', end=datetime.now())
+# Show teh data
+df
+
+
+# In[21]:
+
+
+plt.figure(figsize=(16,6))
+plt.title('Close Price History')
+plt.plot(df['Close'])
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.show()
+
+
+# In[22]:
+
+
+# Create a new dataframe with only the 'Close column 
+data = df.filter(['Close'])
+# Convert the dataframe to a numpy array
+dataset = data.values
+# Get the number of rows to train the model on
+training_data_len = int(np.ceil( len(dataset) * .95 ))
+
+training_data_len
+
+
+# In[23]:
+
+
+# Scale the data
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(dataset)
+
+scaled_data
+
+
+# In[24]:
+
+
+# Create the training data set 
+# Create the scaled training data set
+train_data = scaled_data[0:int(training_data_len), :]
+# Split the data into x_train and y_train data sets
+x_train = []
+y_train = []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+    if i<= 61:
+        print(x_train)
+        print(y_train)
+        print()
+        
+# Convert the x_train and y_train to numpy arrays 
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# x_train.shape
+
+
+# In[25]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+# here i just select epoches 1 to check our model work or not later on we will change 
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+
+# In[26]:
+
+
+# Create the testing data set
+# Create a new array containing scaled values from index 1543 to 2002 
+test_data = scaled_data[training_data_len - 60: , :]
+# Create the data sets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+    
+# Convert the data to a numpy array
+x_test = np.array(x_test)
+
+# Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+
+# Get the models predicted price values 
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# Get the root mean squared error (RMSE)
+rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+rmse
+
+
+# In[27]:
+
+
+# Plot the data
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+# Visualize the data
+plt.figure(figsize=(16,6))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.show()
+
+
+# In[28]:
+
+
+# Show the valid and predicted prices
+valid
+
+
+# In[ ]:
+
+
+
+
